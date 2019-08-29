23e272071bf60374dba7bd0f8a55840c4066cdf0
J.C. Dorr <jcdorr3@JCs-MacBook-Pro.local>
2019-08-29 09:13:42 -0400

    Worked a lot on styling. MVP has been completed since yesterday. Working on Post MVP.

--- brewery-finder/src/components/Home.js
+++ brewery-finder/src/components/Home.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Home = () => {
   return(
     <div className='welcome-container'>
-      <h1 id='welcome'>Wanna get drunk?</h1>
+      <h1 id='welcome1'>Got Beer?</h1>
+      <img id='drunk-sock' src={require('../drunk_sock_puppet.gif')} alt='drunk puppet'/>
+      <img id="drunk-cups" src={require('../drunk_cups.gif')} alt='drunk cups'/>
+      <h1 id='welcome2'>Check out your local breweries!</h1>
+
     </div>
   )
 }

5cbc8286b8ecada447d683efdfe87d01c3a9bf56
J.C. Dorr <jcdorr3@JCs-MacBook-Pro.local>
2019-10-31 15:04:10 -0400

    Updates

---
+++ precache-manifest.288bb20eee50696de76496e3b9009ee4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca8c543127bcd9d7a2248b474117e51",
+    "url": "/brew_find/index.html"
+  },
+  {
+    "revision": "2d1f1f6aeaa086554c73",
+    "url": "/brew_find/static/css/main.204ce3be.chunk.css"
+  },
+  {
+    "revision": "e4c5a633a441e4948e5c",
+    "url": "/brew_find/static/js/2.0bd58d5b.chunk.js"
+  },
+  {
+    "revision": "2d1f1f6aeaa086554c73",
+    "url": "/brew_find/static/js/main.67fed62b.chunk.js"
+  },
+  {
+    "revision": "d1729e208150890eb9e6",
+    "url": "/brew_find/static/js/runtime~main.d36e4360.js"
+  },
+  {
+    "revision": "e48890930e2fcbb9123f47a549cc9fe6",
+    "url": "/brew_find/static/media/Beer icon.e4889093.svg"
+  },
+  {
+    "revision": "de324d001072bcc476c3ee23e1e701e9",
+    "url": "/brew_find/static/media/beer_background.de324d00.jpeg"
+  }
+]);

6e567b1d7a35b20c79d0c4cbc86df0426bb0be66
J.C. Dorr <jcdorr3@JCs-MacBook-Pro.local>
2020-02-18 11:34:18 -0500

    started pwa branch

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,51 +1,53 @@
-import React from 'react';
-import axios from 'axios'
-import {Route, Switch, Redirect} from 'react-router-dom'
-import Home from './Home'
-import BrewerySearch from './BrewerySearch'
+import React from "react";
+import axios from "axios";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Home from "./Home";
+import BrewerySearch from "./BrewerySearch";
 
 class Main extends React.Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
       breweries: []
-    }
+    };
   }
 
   makeAPICall = async () => {
-    axios.get('https://api.openbrewerydb.org/breweries?by_state=new_york')
-    .then(response => {
-    this.setState({
-      breweries: response.data
-    });
-    console.log('this is allData', response.data);
-    })
-    .catch(error => {
-    console.log(error);
-    });
-  }
+    axios
+      .get("https://api.openbrewerydb.org/breweries?by_state=new_york")
+      .then(response => {
+        this.setState({
+          breweries: response.data
+        });
+        console.log("this is allData", response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   componentDidMount() {
     this.makeAPICall();
   }
 
-
-
   render() {
-    console.log(this.state.breweries)
+    console.log(this.state.breweries);
 
     return (
-      <main className='main'>
+      <main className="main">
         <Switch>
-          <Route path='/search' render={(props) => <BrewerySearch {...props}      breweries={this.state.breweries} />} />
-          <Route exact path='/home' component={Home} />
+          <Route
+            path="/search"
+            render={props => (
+              <BrewerySearch {...props} breweries={this.state.breweries} />
+            )}
+          />
+          <Route exact path="/home" component={Home} />
           <Redirect from="/" to="home" />
         </Switch>
       </main>
-    )
+    );
   }
 }
-
 
 export default Main;
